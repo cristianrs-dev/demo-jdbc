ea09b7737720fb4e4ab1cdea6254ce51e6ab17fe
27SARRAZIN <sarrazinr5@gmail.com>
2024-12-09 20:43:32 -0400

    classe Department implementada

---
+++ src/application/program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Department obj = new Department(1, "books");
+		System.out.println(obj);
+	}
+
+}
