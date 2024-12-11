0d337920522b9338124c856168c4db6f76769ff0
27SARRAZIN <sarrazinr5@gmail.com>
2024-12-11 19:23:16 -0400

    implementação da classe Seller

--- src/application/program.java
+++ src/application/program.java
@@ -1,12 +1,17 @@
 package application;
 
+
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Department obj = new Department(1, "books");
+		
+		Seller seller = new Seller(1, "cristian", "cris@gmail.com", new Date() , 3.000, obj);
 		System.out.println(obj);
 	}
 
